7b709c8a62d3a36cde9c61971cca6c6bddde920e
Mahesh <mrak4123@gmail.com>
2023-05-28 10:23:10 -0700

    Added MSAL login

--- components/Layout.js
+++ components/Layout.js
@@ -2,6 +2,7 @@ import { Button, Stack } from '@mui/material';
 import Head from 'next/head';
 import Image from 'next/image';
 import logo from "../public/logo.png";
+import { login } from '../utils/authService';
 
 export const siteTitle = 'LegalAid - Make Scheduling Easy';
 
@@ -25,7 +26,7 @@ export default function Layout({ children, home }) {
                 width={256}
                 alt="LegalAid"
               />
-            <Button variant='contained'>Login</Button>
+            <Button variant='contained' onClick={login}>Login</Button>
           </Stack>
         </header>
         <main>{children}</main>
