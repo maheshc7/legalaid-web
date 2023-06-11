adffafa901961d7b7e70d55ad1231f7fe6760f6f
Mahesh <mrak4123@gmail.com>
2023-06-11 01:14:06 -0700

    Added scopes

--- Config.js
+++ Config.js
@@ -1,7 +1,13 @@
 const config = {
   appId: process.env.NEXT_PUBLIC_APP_ID,
   redirectUri: process.env.NEXT_PUBLIC_APP_REDIRECT_URL,
-  scopes: process.env.NEXT_PUBLIC_APP_SCOPES.split(","),
+  scopes: [
+    "user.read",
+    "Calendars.ReadWrite",
+    "MailboxSettings.Read",
+    "Contacts.Read",
+    "People.Read",
+  ],
   backend_url: process.env.NEXT_PUBLIC_BACKEND_URL,
 };
 
